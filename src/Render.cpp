e1fb89c6bfcfeb762e2de952b5c490dfa209cb17
Jacrispys <jvanzyverden@icloud.com>
2024-05-08 12:09:40 -0700

    parallelization of rendering

--- src/Render.cpp
+++ src/Render.cpp
@@ -17,10 +17,15 @@
 #include "SFML/Graphics/Texture.hpp"
 #include "SFML/Graphics/Font.hpp"
 #include "SFML/Graphics/Text.hpp"
+#include "SFML/Graphics/Sprite.hpp"
 
 void Render::renderWindow(sf::RenderWindow *window) {
     window->setActive(true);
-    sf::VertexArray image = generateVertexArray(window->getSize());
+    sf::Image image = generateVertexArray(window->getSize());
+    sf::Texture fractal;
+    fractal.create(image.getSize().x, image.getSize().y);
+    fractal.update(image);
+    sf::Sprite fractal_sprite(fractal);
     std::cout << "finished" << std::endl;
     sf::Font font;
     if (!font.loadFromFile("../../src/resources/Insanibc.ttf")) {
@@ -52,7 +57,7 @@ void Render::renderWindow(sf::RenderWindow *window) {
         }
         //draw
         window->clear();
-        window->draw(image);
+        window->draw(fractal_sprite);
         window->draw(label);
         // render
         window->display();
@@ -60,10 +65,10 @@ void Render::renderWindow(sf::RenderWindow *window) {
     }
 }
 
-sf::VertexArray Render::generateVertexArray(sf::Vector2u window_size) {
+sf::Image Render::generateVertexArray(sf::Vector2u window_size) {
     const int MAX_ITERATIONS = 500;
     std::array<sf::Vector2f, 32> anti_aliasing_offsets;
-    for (auto& o : anti_aliasing_offsets) {
+    for (auto &o: anti_aliasing_offsets) {
         o.x = RNG(1.0f);
         o.y = RNG(1.0f);
         std::cout << o.x << "," << o.y << std::endl;
@@ -71,38 +76,55 @@ sf::VertexArray Render::generateVertexArray(sf::Vector2u window_size) {
 
     anti_aliasing_offsets[0] = {};
 
-    auto js = JuliaSet(curr_julia, MAX_ITERATIONS);
+    JuliaSet js = JuliaSet(curr_julia, MAX_ITERATIONS);
 
 
-    sf::VertexArray va(sf::Points, (window_size.x * window_size.y));
-    int idx = 0;
+    sf::Image image;
+    image.create(window_size.x, window_size.y);
+
+    int thread_count = 16;
+    int slice_height = window_size.y / thread_count;
+    std::vector<std::thread> thread_pool;
+    for (int k{0}; k < thread_count; k++) {
+        thread_pool.emplace_back(&Render::computeSlice, this, window_size, k, slice_height, anti_aliasing_offsets, MAX_ITERATIONS, std::ref(js), std::ref(image));
+    }
+    for (std::thread &t: thread_pool) {
+        t.join();
+    }
+    return image;
+}
+
+void Render::captureWindow() {
+    this->capture = true;
+}
+
+void Render::computeSlice(sf::Vector2u window_size, int k, int slice_height,
+                          std::array<sf::Vector2f, 32> anti_aliasing_offsets, int MAX_ITERATIONS, JuliaSet &js,
+                          sf::Image &img) {
     for (int x{0}; x < window_size.x; ++x) {
-        for (int y{0}; y < window_size.y; ++y) {
-
-            const int samples = 1;
+        for (int y{k * slice_height}; y < (k + 1) * slice_height; ++y) {
+            const int samples = 16;
             sf::Vertex vertex;
             sf::Vector3f colorSamples;
             for (int i{samples}; i--;) {
                 // Ensure window size x and y are even numbers please for the love of god
                 float scale = static_cast<float>(window_size.y) / 2.0f;
                 sf::Vector2f rand = anti_aliasing_offsets[i];
-                sf::Vector2f curr_coordinate = {(static_cast<float>(x) - (static_cast<float>(window_size.x) / 2.0f)) + rand.x, (static_cast<float>(y) - (static_cast<float>(window_size.y) / 2.0f)) + rand.y};
-                const float julia_iters = js.iterate_coordinate((curr_coordinate / scale));
+                sf::Vector2f curr_coordinate = {
+                        (static_cast<float>(x) - (static_cast<float>(window_size.x) / 2.0f)) + rand.x,
+                        (static_cast<float>(y) - (static_cast<float>(window_size.y) / 2.0f)) + rand.y};
+                const float julia_iters = js.smooth_iterate((curr_coordinate / scale));
                 const float iter_ratio = (julia_iters / static_cast<float>(MAX_ITERATIONS));
                 vertex.position = {static_cast<float>(x), static_cast<float>(y)};
                 sf::Vector3f c = Gradients::applyGradient(iter_ratio, Gradient::DARK_MULTICOLOR);
                 colorSamples += c;
             }
             sf::Vector3f finalColor = colorSamples / float(samples);
-            vertex.color = Gradients::vec3ToColor(finalColor);
-            va[idx++] = vertex;
+            sf::Color color = Gradients::vec3ToColor(finalColor);
+            img.setPixel(x, y, color);
+
         }
     }
-    return va;
-}
-
-void Render::captureWindow() {
-    this->capture = true;
 }
 
 
--- src/Render.hpp
+++ src/Render.hpp
@@ -9,13 +9,14 @@
 #include <random>
 #include "SFML/Graphics/RenderWindow.hpp"
 #include "SFML/Graphics/VertexArray.hpp"
+#include "utils/JuliaSet.hpp"
 
 class Render {
 
 public:
     Render() {
         mt = std::mt19937(1);
-        curr_julia = {-0.34f, 0.62f};
+        curr_julia = {-0.71f, 0.25f};
     }
 
     std::thread startRenderThread(sf::RenderWindow &window);
@@ -30,12 +31,16 @@ private:
 
     bool capture = false;
 
-    sf::VertexArray generateVertexArray(sf::Vector2u window_size);
+    sf::Image generateVertexArray(sf::Vector2u window_size);
     std::mt19937 mt;
     float RNG(float max) {
         std::uniform_real_distribution<float> dist(0.0f, max);
         return dist(mt);
     }
+
+    void
+    computeSlice(sf::Vector2u window_size, int k, int slice_height, std::array<sf::Vector2f, 32> anti_aliasing_offsets,
+                 int MAX_ITERATIONS, JuliaSet &js, sf::Image &img);
 };
 
 
